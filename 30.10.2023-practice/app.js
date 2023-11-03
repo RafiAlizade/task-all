e06337f48d861cd05169389f0f796a114f1c6967
Rafi Alizade <refielizade3@gmail.com>
2023-11-03 21:13:23 +0400

    03.11.2023

---
+++ 30.10.2023-practice/app.js
@@ -0,0 +1,62 @@
+// alert("Welcome to Login panel!")
+
+console.log(false)
+console.log(true)
+
+console.warn("This website is maintaining now.. Please quit and try again later")
+
+// // ! Dəyişən
+
+// var a = 10;
+// var b = 15;
+// var c = 20;
+
+
+
+// console.log(a,b,c);
+
+// ! Primitive
+
+// var a = 15;
+// var b = 30;
+// var c = 250;
+
+// console.log(typeof a);
+// console.log(typeof b);
+// console.log(typeof c);
+
+// // ! Boolean
+
+// var a = false;
+
+// console.log(typeof a);
+
+// ! Undefined
+
+var a;
+
+console.log(typeof a);
+
+//! Null
+
+// var a = null;
+
+// console.log(typeof a);
+
+
+// ! Reference
+
+// var numbers = [1,2,3,4,5,6,7,8,9]
+
+// console.log(numbers);
+// console.log(typeof numbers);
+
+var person = {
+    name: "Rafi",
+    age : 22,
+    country: "Azerbaijan",
+};
+
+console.log(person)
+console.log(typeof person)
+
